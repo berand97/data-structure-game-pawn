0974e30f21cb43599c645847aa12ec4385c63aa3
Kranber <97bermudez.andres@gmail.com>
2020-02-24 11:42:51 -0500

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,10 @@
 import com.unac.edu.co.Ajedrez;
 
 public class Main {
-    final int N = 8;
-    final int n = (N+1);
-    static boolean solucion;
-    static boolean primero = false;
-
     public static void main(String[] args) {
-        /*int [][] tablero = new int [8][8];
+        Ajedrez object = new Ajedrez();
+            object.ponerPeon(object.j);
+            System.out.println("los exitos de la posicion del peon " + " son "+ object.contador  );
 
-        for (int x=0; x < tablero.length; x++) {
-            System.out.print("|");
-            for (int y=0; y < tablero[x].length; y++) {
-                System.out.print (tablero[x][y]);
-                if (y!=tablero[x].length-1) System.out.print("\t");
-            }
-            System.out.println("|");
-        }*/
     }
-
-    private void ponerAlfil(int i){
-        int j;
-        j=1;
-        int alfil;
-        do{
-            alfil=j;
-
-
-        }while(!solucion && (j <= 8));
-    }
-
-
-
-
-
-
-
 }
--- src/com/unac/edu/co/Ajedrez.java
+++ src/com/unac/edu/co/Ajedrez.java
@@ -1,30 +1,33 @@
 package com.unac.edu.co;
 
 public class Ajedrez {
-    final int N = 8;
-    final int n = (N+1);
-    //int [] reinas = new int[n];
-    static boolean solucion;
-    //static boolean primero = false;
+    static final int N = 8;
+    static final int s = (N+1);
+    final int  x0=5;
+    final int  y0=8;
+    public final int j=1;
+    public int contador=0;
+    boolean visitado[]= new boolean[9];
+    public void ponerPeon(int z){
+        boolean exito=true;
+        visitado[z]=true;
+        for(int i=1; i<9; i++){
+            if(Math.abs(x0-i) == Math.abs(y0-z)){
+                exito=false;
+                break;
+            }
+       }
+        if(z<8 && !visitado[z+1]) {
+            ponerPeon(z + 1);
 
-    public boolean solucionPeon() {
-        solucion = false;
-        ponerPeon(1);
-        return solucion;
-    }
-
-    private void ponerPeon(int i)	{
-    }
-
-    private boolean valido(int i)		{
         }
-        return ;
+        if(z>1&& !visitado[z-1]){
+            ponerPeon(z-1);
+        }
+        if(exito){
+            contador++;
+
+        }
+
     }
-
-    void escribirReinas()	{
-
-    }
-
-
-
 }
