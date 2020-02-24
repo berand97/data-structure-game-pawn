151731199b32c07ffbea3b9bac827c20a4f949d9
Kranber <97bermudez.andres@gmail.com>
2020-02-24 08:49:56 -0500

    second commit

---
+++ src/com/unac/edu/co/Ajedrez.java
@@ -0,0 +1,30 @@
+package com.unac.edu.co;
+
+public class Ajedrez {
+    final int N = 8;
+    final int n = (N+1);
+    //int [] reinas = new int[n];
+    static boolean solucion;
+    //static boolean primero = false;
+
+    public boolean solucionPeon() {
+        solucion = false;
+        ponerPeon(1);
+        return solucion;
+    }
+
+    private void ponerPeon(int i)	{
+    }
+
+    private boolean valido(int i)		{
+        }
+        return ;
+    }
+
+    void escribirReinas()	{
+
+    }
+
+
+
+}
--- src/com/unac/edu/co/Main.java
+++
@@ -1,8 +0,0 @@
-package com.unac.edu.co;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
